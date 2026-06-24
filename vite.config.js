f3d3523307b3edee2161e8a981b082ce7fb00db3
Ibrahim2144 <kolawoleibrahim100@gmail.com>
2026-06-24 01:23:50 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/My-portfolio-website/",
+  base: "/My-portfolio-website",
   plugins: [
     react(),
     tailwindcss(),
